0009232cab7582a0006df11007d6fdcafb15f023
Ivan Kuznetsov <dig88@yandex.ru>
2022-10-21 13:34:41 +0300

    Correction of comments

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,9 @@
 package jm.task.core.jdbc.util;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+
 
 public class Util {
     // реализуйте настройку соеденения с БД
